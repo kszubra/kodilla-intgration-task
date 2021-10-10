18d9e4b6427991d03a792e9df491fd82ce0a0581
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-10 13:32:53 +0200

    first commit

---
+++ src/main/java/com/kodilla/task/kodillaintegrationtask/KodillaIntegrationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.kodillaintegrationtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaIntegrationTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaIntegrationTaskApplication.class, args);
+    }
+
+}
